8419d5a99a4ebe91b3e94acfd727f431e333052f
kaar20 <codewithkar@gmail.com>
2024-11-21 16:12:56 +0300

    Shopping App

---
+++ app/src/main/java/com/example/day7/ShoppingList.kt
@@ -0,0 +1,99 @@
+package com.example.day7
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+data class ShoppingItem(
+    val id:Int,
+    var name:String,
+    var quantity:Int,
+    var isEditing:Boolean=false
+
+)
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+
+fun ShoppingListApp(){
+    var sItems by remember{ mutableStateOf(listOf<ShoppingItem>()) }
+    var showDiaglog by remember {  mutableStateOf(false)}
+    var itemName by remember {
+        mutableStateOf("")
+    }
+    var itemQuantity by remember {
+        mutableStateOf("")
+    }
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Button(onClick = {showDiaglog = true },
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        ) {
+            Text(text = "Add Item")
+        }
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
+            items(sItems){
+
+            }
+            
+
+
+
+        }
+
+    }
+   if(showDiaglog)
+
+   {
+       AlertDialog(onDismissRequest = { showDiaglog = false },
+           confirmButton = {},
+           title = {Text(text = "Add Shopping Item")},
+           text = {
+               Column {
+                   OutlinedTextField(value = itemName, onValueChange = {
+                       itemName = it
+                   },
+
+                       singleLine = true,
+                       modifier = Modifier.fillMaxWidth().padding(8.dp)
+                       )
+
+                   OutlinedTextField(value = itemQuantity, onValueChange = {
+                       itemQuantity = it
+                   },
+
+                       singleLine = true,
+                       modifier = Modifier.fillMaxWidth().padding(8.dp)
+                   )
+               }
+           }
+
+
+//           title = {}
+//           Text(text = "Mohamed You Are Doing Great")
+
+       )}
+}
